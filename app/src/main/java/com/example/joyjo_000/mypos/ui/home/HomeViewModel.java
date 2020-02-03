f4957788c044fc818d52832cddfbe1503fa2df01
joyjo_000 <joyjoyolata@gmail.com>
2020-02-03 16:32:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/joyjo_000/mypos/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.joyjo_000.mypos.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
